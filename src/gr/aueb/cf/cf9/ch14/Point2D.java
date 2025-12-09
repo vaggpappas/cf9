e3d9243edbca7748d3f9badf4574a1ab7eb6b93d
vaggpappas <vag_pappas@hotmail.com>
2025-12-09 19:06:12 +0200

    feat: add point2D derived class

---
+++ src/gr/aueb/cf/cf9/ch14/Point2D.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.cf9.ch14;
+
+public class Point2D extends Point {
+    private double y;
+
+    public Point2D () {
+//        super();
+//        y = 0;
+    }
+
+    public Point2D (double x, double y) {
+        super(x);
+        this.y = y;
+    }
+
+    @Override
+    public void movePlusOne() {
+        super.movePlusOne();
+        y += 1;
+    }
+
+    @Override
+    protected void movePlus10() {
+        super.movePlus10();
+        y += 10;
+    }
+}
