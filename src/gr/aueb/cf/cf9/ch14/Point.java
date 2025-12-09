6a3f3b27b15107ae16bc3026f43e7255f45aa07f
vaggpappas <vag_pappas@hotmail.com>
2025-12-09 18:47:55 +0200

    feat: add point class

---
+++ src/gr/aueb/cf/cf9/ch14/Point.java
@@ -0,0 +1,31 @@
+package gr.aueb.cf.cf9.ch14;
+
+public class Point {
+    private double x;
+
+    public Point () {}
+
+    public Point (double x) {
+        this.x = x;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void movePlusOne () {
+        x += 1;
+    }
+
+    protected void movePlus10 () {
+        x += 10;
+    }
+
+    protected void reset () {
+        x = 0;
+    }
+}
