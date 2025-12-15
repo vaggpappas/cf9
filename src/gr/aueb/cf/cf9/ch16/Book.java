e77f74f96e457dc62d4caa3a5cb160a040e1a4a5
vaggpappas <vag_pappas@hotmail.com>
2025-12-15 19:21:24 +0200

    feat: add Builder Design Pattern

---
+++ src/gr/aueb/cf/cf9/ch16/Book.java
@@ -0,0 +1,58 @@
+package gr.aueb.cf.cf9.ch16;
+
+public class Book {
+    private final long id;
+    private final String title;
+    private final String author;
+    private final String isbn;
+
+    private Book(Builder builder) {
+        this.id = builder.id;
+        this.title = builder.title;
+        this.author = builder.author;
+        this.isbn = builder.isbn;
+    }
+
+    public static class Builder {
+        private final long id;
+        private final String isbn;
+        private String title = "";
+        private String author = "";
+
+        public Builder(long id, String isbn) {
+            this.id = id;
+            this.isbn = isbn;
+        }
+
+        // Fluent Setter - returns the Builder object - chaining
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder author(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public Book build() {
+            return new Book(this);
+        }
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+}
---
+++ src/gr/aueb/cf/cf9/ch16/Main.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.cf9.ch16;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Book book1 = new Book.Builder(1, "A123").build();
+        Book book2 = new Book.Builder(2, "B456").author("Androutsos").title("Java").build();
+        Book book3 = new Book.Builder(3, "C456").title("javascript").build();
+    }
+}
