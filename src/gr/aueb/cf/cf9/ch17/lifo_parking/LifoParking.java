9cbf6023c87654573c9e9b670e35a04a6528c8c4
vaggpappas <vag_pappas@hotmail.com>
2025-12-22 18:21:47 +0200

    feat: add lifo parking example

---
+++ src/gr/aueb/cf/cf9/ch17/lifo_parking/LifoParking.java
@@ -0,0 +1,39 @@
+package gr.aueb.cf.cf9.ch17.lifo_parking;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LifoParking {
+    private final Deque<String> parking = new LinkedList<>();
+
+    public LifoParking() {
+
+    }
+
+    public List<String> getParking() {
+        return List.copyOf(parking);
+    }
+
+    // Delegation
+    public void addCar(String car) {
+        parking.push(car);
+        System.out.println(LocalDateTime.now() + ": " + ", Είσοδος, Αρ. Πινακίδας=" + car);
+    }
+
+    public String removeCar() {
+        String car = parking.pop();
+        System.out.println(LocalDateTime.now() + " Έξοδος, Αρ. Πινακίδας=" + car);
+        return car;
+    }
+
+    public boolean isEmpty() {
+        return parking.isEmpty();
+    }
+
+    public void traverse() {
+        parking.forEach(System.out::println);
+    }
+}
