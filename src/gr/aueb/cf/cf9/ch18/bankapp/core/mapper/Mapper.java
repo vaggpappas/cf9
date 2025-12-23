9b00d7a63cfe7a2729f20c47ec73f15e6fb2748a
vaggpappas <vag_pappas@hotmail.com>
2025-12-23 20:50:20 +0200

    feat(mapper): add mapper

---
+++ src/gr/aueb/cf/cf9/ch18/bankapp/core/mapper/Mapper.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.cf9.ch18.bankapp.core.mapper;
+
+import gr.aueb.cf.cf9.ch18.bankapp.dto.AccountInsertDTO;
+import gr.aueb.cf.cf9.ch18.bankapp.dto.AccountReadOnlyDTO;
+import gr.aueb.cf.cf9.ch18.bankapp.model.Account;
+
+public class Mapper {
+    /**
+     * No instances of this class should be available.
+     */
+    private Mapper() {
+
+    }
+
+    public static Account mapToModelEntity(AccountInsertDTO dto) {
+        return new Account(dto.iban(), dto.balance());
+    }
+
+    public static AccountReadOnlyDTO mapToReadOnlyDTO(Account account) {
+        return new AccountReadOnlyDTO(account.getIban(), account.getBalance());
+    }
+}
